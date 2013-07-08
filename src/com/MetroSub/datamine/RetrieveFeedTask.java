f6b576a26654367e90e7619da1b8daec0bde088a
Kush Patel <kushpatel@Kushs-MacBook-Pro.local>
2013-07-07 20:45:43 -0400

    nyct feed parsing examples with documented comments

--- src/com/MetroSub/datamine/RetrieveFeedTask.java
+++ src/com/MetroSub/datamine/RetrieveFeedTask.java
@@ -8,7 +8,6 @@ import com.google.transit.realtime.NyctSubway;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -45,44 +44,82 @@ public class RetrieveFeedTask extends AsyncTask<InputStream, Void, ByteString> {
 
         //TODO (Kush): parse data feed using protocol buffer libraries
 
-
+        // Remember to add NYCT registries to the extension registry which is then passed as parameter in GTFS parsing
         ExtensionRegistry registry = ExtensionRegistry.newInstance();
         NyctSubway.registerAllExtensions(registry);
 
         GtfsRealtime.FeedMessage feedMessage = null;
-        //GtfsRealtime.TripDescriptor tripDescriptor = null;
-        //NyctSubway.NyctFeedHeader nyctFeedHeader = null;
-        //NyctSubway.NyctTripDescriptor nyctTripDescriptor = null;
-        //NyctSubway.NyctStopTimeUpdate nyctStopTimeUpdate = null;
-        //NyctSubway.TripReplacementPeriod tripReplacementPeriod = null;
         try {
+            // parse GTFS data using GTFS real-time library
             feedMessage = GtfsRealtime.FeedMessage.parseFrom(data,registry);
-            //nyctFeedHeader = NyctSubway.NyctFeedHeader.parseFrom(data);
-            //nyctTripDescriptor = NyctSubway.NyctTripDescriptor.parseFrom(data);
-            //nyctStopTimeUpdate = NyctSubway.NyctStopTimeUpdate.parseFrom(data);
-            //tripReplacementPeriod = NyctSubway.TripReplacementPeriod.parseFrom(data);
 
         } catch (InvalidProtocolBufferException e) {
             Log.e(TAG,"InvalidProtocolBufferException: " + e.getMessage());
         }
 
-        Log.d(TAG,"Header realtime version = " + feedMessage.getHeader().getGtfsRealtimeVersion());
-        for (int i = 0; i < feedMessage.getEntityCount(); i++) {
+        /**
+         * FeedMessage has: 1) FeedHeader
+         *                  2) FeedEntity
+         **/
+        GtfsRealtime.FeedHeader feedHeader = feedMessage.getHeader();
+        GtfsRealtime.FeedEntity feedEntity_1 = feedMessage.getEntity(0);
+
+
+        /***************************************************************************************************************
+         * FeedHeader has: 1) gtfs_realtime_version
+         *                 2) incrementally
+         *                 3) timestamp
+         *                 4) extension = nyctFeedHeader
+         **/
+        Log.d(TAG,"Header realtime version = " + feedHeader.getGtfsRealtimeVersion());
+
+        /**
+         * Extension = nyctFeedHeader has: 1) nyct_subway_version
+         *                                 2) trip_replacement_period
+         **/
+        Log.d(TAG,"Nyct subway version = " + feedHeader.getExtension(NyctSubway.nyctFeedHeader).getNyctSubwayVersion());
+        /**
+         * trip_replacement_period has: 1) route_id
+         *                              2) replacement_period
+         **/
+        Log.d(TAG,"Trip replacement id for index 1 = " + feedHeader.getExtension(NyctSubway.nyctFeedHeader)
+                                                            .getTripReplacementPeriod(0).getRouteId());
+
+
+
+        /***************************************************************************************************************
+         * FeedEntity has: 1) id
+         *                 2) trip_update
+         *                 3) vehicle
+         *                 4) alert
+         **/
+        Log.d(TAG,"Entity 1, id = " + feedEntity_1.getId());
+
+
+        /**
+         * trip_update has: 1) trip : trip_id, route_id, start_time, start_date, schedule_relationship,
+         *                            extension = nyct_trip_descriptor: train_id, is_assigned, direction (enum Direction)
+         *                  2) stop_time_update(@count): stop_id, arrival, departure,
+         *                                               extension = nyct_stop_time_update: scheduled_track, actual_track
+         */
+        GtfsRealtime.TripUpdate tripUpdate = feedEntity_1.getTripUpdate();
+
+        GtfsRealtime.TripDescriptor tripDescriptor = tripUpdate.getTrip();
+        Log.d(TAG, "Entity 1, trip id = " + tripDescriptor.getTripId());
+        Log.d(TAG,"Entity 1, train id = " + tripDescriptor.getExtension(NyctSubway.nyctTripDescriptor).getTrainId());
+
+        Log.d(TAG,"Entity 1, number of stations = " + tripUpdate.getStopTimeUpdateCount());
+        GtfsRealtime.TripUpdate.StopTimeUpdate stopTimeUpdate_1 = tripUpdate.getStopTimeUpdate(0);
+        Log.d(TAG, "Entity 1, 1st stop id = " + stopTimeUpdate_1.getStopId());
+        Log.d(TAG,"Entity 1, 1st stop id's actual track = " +
+                            stopTimeUpdate_1.getExtension(NyctSubway.nyctStopTimeUpdate).getActualTrack());
+
+
+
+
+
+        /*for (int i = 0; i < feedMessage.getEntityCount(); i++) {
             Log.d(TAG,"Entity stop id at " + i + " = " + feedMessage.getEntity(i).getTripUpdate().getTrip().getTripId());
-        }
-
-        /*Log.d(TAG,"Trip id = " + tripDescriptor.getTripId());
-        Log.d(TAG,"Route id = " + tripDescriptor.getRouteId());
-        Log.d(TAG,"Start time = " + tripDescriptor.getStartTime());
-        Log.d(TAG, "Start date = " + tripDescriptor.getStartDate());
-        Log.d(TAG, "Start date present = " + tripDescriptor.hasStartDate());
-        Log.d(TAG, "Route id present = " + tripDescriptor.hasRouteId()); */
-
-
-        /*List<Descriptors.FieldDescriptor> fieldDescriptors = nyctTripDescriptor.getDescriptorForType().getFields();
-        for (Descriptors.FieldDescriptor descriptor : fieldDescriptors) {
-            String name = descriptor.getFullName();
-            Log.d(TAG,"Descriptor name = " + name);
         }*/
 
     }
