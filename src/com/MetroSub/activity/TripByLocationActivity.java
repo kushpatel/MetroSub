c897716aacf47b27c4c6b97d49d8a266e3c6c83d
Shenil Dodhia <shenil.dodhia@yale.edu>
2013-07-14 16:28:09 -0400

    sample code to display a basic marker

--- src/com/MetroSub/activity/TripByLocationActivity.java
+++ src/com/MetroSub/activity/TripByLocationActivity.java
@@ -2,6 +2,11 @@ package com.MetroSub.activity;
 
 import android.os.Bundle;
 import com.MetroSub.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,10 +16,30 @@ import com.MetroSub.R;
  * To change this template use File | Settings | File Templates.
  */
 public class TripByLocationActivity extends BaseActivity {
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        if (map!=null){
+            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                    .title("Hamburg"));
+            Marker kiel = map.addMarker(new MarkerOptions()
+                    .position(KIEL)
+                    .title("Kiel")
+                    .snippet("Kiel is cool"));
+            //.icon(BitmapDescriptorFactory
+            //      .fromResource(R.drawable.ic_launcher)));
+        }
+       // setContentView(R.layout.trip_location);
+
     }
 }
